ce5a5c8d1951d963584478c6a79502d47e32782d
Travis-Gray2194 <bmknigussie@gmail.com>
2018-01-30 16:38:29 -0500

    Initial commit

---
+++ src/main/java/com/bisratmk/springboot_06/springboot_06/HomeController.java
@@ -0,0 +1,28 @@
+package com.bisratmk.springboot_06.springboot_06;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+
+public class HomeController {
+
+
+    @GetMapping("/songform")
+    public String loaFormPage(Model model){
+
+        model.addAttribute("song",new Song());
+        return "songform";
+
+    }
+    @PostMapping("/songform")
+    public String loadFromPage(@ModelAttribute Song song, Model model){
+
+        model.addAttribute("song", song);
+        return "confirmsong";
+    }
+}
